3927c65d5a796af053c0dd99f0dc3f020e10cb87
amadou <adiattara@gmail.com>
2024-10-19 01:45:49 +0200

    update feature engineering by tokenising and vectorizing with one hote encoding

--- src/feature.py
+++ src/feature.py
@@ -1,7 +1,22 @@
-def make_features(df):
-    y = df["is_comic"]
+import joblib
 
-    # Fake X: constant equal to 0
-    X = [[0]] * len(y)
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+
+def make_features(df,fit = True):
+
+
+
+    if fit:
+        vectorizer = CountVectorizer()
+        X = vectorizer.fit_transform(df['video_name'])
+        joblib.dump(vectorizer, 'models/vectorized.pkl')
+
+    else:
+        vectorizer = joblib.load('models/vectorized.pkl')
+        X = vectorizer.transform(df['video_name'])
+
+    y = df['is_comic'].values
 
     return X, y
