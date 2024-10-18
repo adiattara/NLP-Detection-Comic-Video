3a15420b922f7dd9d98fc04f60f6207536098533
amadou <adiattara@gmail.com>
2024-10-19 01:49:27 +0200

    update models.py to add several other model like decision tree,baseline model

--- src/models.py
+++ src/models.py
@@ -1,6 +1,21 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.dummy import DummyClassifier
 
 
-def make_model():
-    return RandomForestClassifier()
+def make_baseline():
+    return DummyClassifier(strategy='most_frequent')
+
+def make_random_forest():
+    return RandomForestClassifier(max_depth=5,class_weight='balanced',n_estimators=100)
+
+def make_logistic_regression():
+    return LogisticRegression(class_weight='balanced',max_iter=1000)
+
+def make_naive_bayes():
+    return MultinomialNB()
+
+def make_decision_tree():
+    return DecisionTreeClassifier(max_depth=5,class_weight='balanced')
