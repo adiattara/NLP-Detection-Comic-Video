a038e888750e19371c6109a7688fa0e93e350744
amadou <adiattara@gmail.com>
2024-10-20 00:57:34 +0200

    reduction de bruit + lemmatisation

--- src/feature.py
+++ src/feature.py
@@ -1,15 +1,33 @@
+import string
+
 import joblib
+import spacy
+from nltk.corpus import stopwords
 
-from sklearn.feature_extraction.text import CountVectorizer
-
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 
 def make_features(df,fit = True):
+    # reduction de bruit
+    df['video_name'] = df['video_name'].str.replace(f'[{string.punctuation}]', ' ', regex=True)
 
+    french_stopwords = set(stopwords.words('french'))
 
+    df['video_name'] = df['video_name'].apply(
+        lambda x: ' '.join([word for word in x.split() if word not in french_stopwords])
+    )
+
+    nlp = spacy.load('fr_core_news_sm')
+
+    #lemmatisation
+    df['video_name'] = df['video_name'].apply(
+
+        lambda x: ' '.join([token.lemma_ for token in nlp(x)])
+    )
 
     if fit:
-        vectorizer = CountVectorizer()
+
+        vectorizer = CountVectorizer(ngram_range=(1, 2),max_features=5000)
         X = vectorizer.fit_transform(df['video_name'])
         joblib.dump(vectorizer, 'models/vectorized.pkl')
 
